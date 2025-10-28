b2b58e8f1a744fb2313704a783c0947a4ce071b8
AbdelrahmanMoustafa <podimustafa05@gmail.com>
2025-10-28 16:01:55 +0300

    Adaline Model
    Fixed the old Numpy version

--- Task 1/adaline.py
+++ Task 1/adaline.py
@@ -27,7 +27,7 @@ class AdalineModel:
         self.mse_threshold = mse_threshold
         self.add_bias = add_bias
         self.weights = None
-        self.bias = np.float_(0)
+        self.bias = np.float64(0)
         self.losses = []
 
     def train(self, X_train, y_train):
@@ -42,7 +42,7 @@ class AdalineModel:
             self: Returns the trained model object
         """
         self.weights = np.zeros(X_train.shape[1])
-        self.bias = np.float_(0)
+        self.bias = np.float64(0)
         
         self.losses = []
         epoch = 0
